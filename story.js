8c2f6dcd6702e1678db20006d14e06b7f8fcef7b
quantumpotato <quantumpotato@gmail.com>
2016-01-12 17:49:34 -0500

    Split text

--- story.js
+++ story.js
@@ -5,41 +5,44 @@ class Story {
 		this.story = 
 			[
 
-["I spot my friend by his green and white plaid shirt stained with matching paint resting over his relaxed slouch at the bar. He sips a mocha latte out of a ocean blue ceramic mug and I smile, happy to see familiar signs. I hang my coat on the chair, order the daily roast and slide into the chair next to him.",
+["I spot my friend by his green and white plaid shirt stained with matching paint resting over his relaxed slouch at the bar. He sips a mocha latte out of a ocean blue ceramic mug and I smile, happy to see familiar signs.",
 [["Coffee", 1]]],
 
-["'Hey buddy. I found a dog today. Healthy looking pup, just looked a little lost off 13th.'",
+["I hang my coat on the chair, order the daily roast and slide into the chair next to him.",
 [["Coffee", 2]]],
 
+["'Hey buddy. I found a dog today. Healthy looking pup, just looked a little lost off 13th.'",
+[["Coffee", 3]]],
+
 ["'Oh yah? My daughter's asked me for a dog. Hard enough for her to find playmates since her...'",
-[["Give dog.", 3], ["Think about it.", 6]]],
+[["Give dog.", 4], ["Think about it.", 7]]],
 
 ["Black life winds out of its bowl, dripping into my mug. From Venezuela, dark with a hint of citrus. It smells like ambition and lifts me out of my haze.",
-[["Coffee", 4]]],
+[["Coffee", 5]]],
 
 ["'Give the dog to your child. He ran away - let him meet a new friend.'\n\n'Thanks, coffee man. Come by tonight?'\n\n'Always nice to see your family. I'll bring her new pal.'",
-[["Coffee", 5]]],
+[["Coffee", 6]]],
 
 ["I sip from Venezuela's roast and imagine what else I can transform today.",
 []],
 
 ["Memories of ernest harvest and a striving for peace glide from the smooth roast over my pallete as a gentle tap returns me to my seat.",
-[["Coffee", 7]]],
-
-["'Excuse me, sir? I heard you found a puppy -- off 13th? Sort of beige with white spots?'",
 [["Coffee", 8]]],
 
-["I turn to face the voice, a woman lifting her wide-brimmed hat the shade of my friend's mug to look questioningly into my eyes.",
+["'Excuse me, sir? I heard you found a puppy -- off 13th? Sort of beige with white spots?'",
 [["Coffee", 9]]],
 
-["'That's yours? Glad you found me. Please sit, share a drink with us. I'll walk your dog here after.'",
+["I turn to face the voice, a woman lifting her wide-brimmed hat the shade of my friend's mug to look questioningly into my eyes.",
 [["Coffee", 10]]],
 
-["She smiles, sits next to me, orders an expresso then turns to face me.",
+["'That's yours? Glad you found me. Please sit, share a drink with us. I'll walk your dog here after.'",
 [["Coffee", 11]]],
 
-["'I thought he got lost when he ran out this morning. Patching up the walls after the storm spooked him. He's a simple creature, but he keeps me happy.'",
+["She smiles, sits next to me, orders an expresso then turns to face me.",
 [["Coffee", 12]]],
+
+["'I thought he got lost when he ran out this morning. Patching up the walls after the storm spooked him. He's a simple creature, but he keeps me happy.'",
+[["Coffee", 13]]],
 
 ["I sip from Venezuela's roast and imagine what else I can transform today.",
 []],
